a300f504b6499b23076dd7c27f7ba43bc2badf4d
wyn <k022298@gmail.com>
2024-04-14 20:52:23 +0100

    Added models for Toilet, added migrate, route for get and add toilet

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
-# from models import db, Toilet
+from models import db, Toilet
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 # Database
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://mydb.db'
-db.init.app(app)
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/toilets', methods=['GET'])
-def get_toilets();
+def get_toilets():
+    toilets = Toilet.query.all()
+    return jsonify([toilet.to_dict() for toilet in toilets])
+
+@app.route('/toilets', methods=['POST'])
+def add_toilet():
+    toilet_data = request.json
+    toilet = Toilet(
+        name=toilet_data['name'],
+        latitude=toilet_data['latitude'],
+        longitude=toilet_data['longitude'],
+        accessibility=toilet_data.get('accessibility', ''),
+        description=toilet_data.get('description', '')
+    )
+    db.session.add(toilet)
+    db.session.commit()
+    return jsonify(toilet.to_dict()), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
